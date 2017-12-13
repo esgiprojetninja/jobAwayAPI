28edf584f7373478cb4153a91112bab7dc965364
GoRFy <lambot.rom@gmail.com>
2017-12-13 16:43:47 +0000

    delete useless comment

--- src/AppBundle/Action/UserSubscribe.php
+++ src/AppBundle/Action/UserSubscribe.php
@@ -53,11 +53,6 @@ class UserSubscribe
         $errors = $this->validator->validate($user);
 
         if (count($errors) > 0) {
-            /*
-             * Uses a __toString method on the $errors variable which is a
-             * ConstraintViolationList object. This gives us a nice string
-             * for debugging.
-             */
             $errorsString = (string) $errors;
 
             return new Response($errorsString);
