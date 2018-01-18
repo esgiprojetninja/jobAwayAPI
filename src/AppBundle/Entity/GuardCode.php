22999285891ff2d8612302038e72c452ccf64ff6
Roland Kullet <renaud.bellec.3@gmail.com>
2018-01-18 16:16:07 +0100

    Guard: create GuardCode

--- src/AppBundle/Entity/GuardCode.php
+++ src/AppBundle/Entity/GuardCode.php
@@ -3,11 +3,19 @@
 namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
- * @ApiResource
+ * @ApiResource(attributes={
+ *     "normalization_context"={"groups"={"read"}},
+ *     "denormalization_context"={"groups"={"write"}}
+ * },
+ *      collectionOperations={
+ *          "create"={"route_name"="create_guard"}
+ *     }
+ * )
  * @ORM\Table(name="guard_code")
  */
 class GuardCode
@@ -15,48 +23,61 @@ class GuardCode
     public function __construct() {
         $this->updatedAt = new \DateTime();
         $this->createdAt = new \DateTime();
+        $codeArray = [];
+        for($i = 0; $i < 6; $i++) {
+           $codeArray[] = rand(0, 10);
+        }
+        $this->code = join("", $codeArray);
+        $this->nbAttempts = 3;
+        $now = new \DateTime();
+        $this->validityDateTime = $now->add(new \DateInterval("PT15M"));
     }
 
     /**
      * @ORM\Id
      * @ORM\Column(name="id", type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Groups({"read"})
      */
     protected $id;
 
     /**
      * @ORM\ManyToOne(targetEntity="User", cascade={"persist"})
      * @ORM\JoinColumn(name="user", referencedColumnName="id", onDelete="SET NULL")
+     * @Groups({"read"})
      */
     protected $user;
 
     /**
      * @ORM\Column(name="code", type="integer")
-     * @Assert\NotBlank()
+     * @Groups({"read"})
      */
     protected  $code;
 
     /**
      * @ORM\Column(name="validity_datetime", type="datetime")
      * @Assert\DateTime()
+     * @Groups({"read"})
      */
     protected $validityDateTime;
 
     /**
      * @ORM\Column(name="nbAttempts", type="integer")
-     * @Assert\NotBlank()
+     * @Groups({"read"})
      */
     protected $nbAttempts;
 
     /**
      * @ORM\Column(name="created_at", type="datetime")
      * @Assert\DateTime()
+     * @Groups({"read"})
      */
     protected $createdAt;
 
     /**
      * @ORM\Column(name="updated_at", type="datetime")
      * @Assert\DateTime()
+     * @Groups({"read"})
      */
     protected $updatedAt;
 
