1f53b026d1aad44bed83ce597d6b560d1167b4b6
GoRFy <lambot.rom@gmail.com>
2018-01-18 08:57:02 +0000

    PATCH method POST and PUT

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,21 +2,9 @@
 
 namespace AppBundle;
 
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new class implements CompilerPassInterface {
-            public function process(ContainerBuilder $container) {
-                $container
-                    ->findDefinition('api_platform.listener.request.deserialize')
-                    ->clearTags();
-            }
-        });
-    }
+
 }
