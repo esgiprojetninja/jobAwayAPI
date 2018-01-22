39c7a361406e5844d41361519c832919a97e586a
teddy meksavanh <ventialaw@hotmail.fr>
2018-01-22 20:23:52 +0100

    Add join to fetch missions by city

--- src/AppBundle/Action/MissionSearch.php
+++ src/AppBundle/Action/MissionSearch.php
@@ -10,44 +10,39 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManager;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class MissionSearch
 {
 
     protected $em;
 
-    public function __construct(EntityManager $em, ValidatorInterface $validator)
+    public function __construct(EntityManager $em)
     {
         $this->em = $em;
-        $this->validator = $validator;
     }
 
     /**
      * @Route(
      *     name="mission_search",
-     *     path="/api/missions",
+     *     path="/api/missions/search",
      *     defaults={"_api_resource_class"=Mission::class, "_api_collection_operation_name"="mission_search"}
      * )
      * @Method("POST")
      * @param Request $request
-     * @return User|JsonResponse|Response
+     * @return Mission|JsonResponse|Response
      * @throws \Doctrine\ORM\OptimisticLockException
      */
+
     public function __invoke(Request $request)
     {
-        $mission = new Mission();
+        $missionsByCity = $em
+            ->getRepository('AppBundle:Entity:Accommodation')
+            ->createQueryBuilder('a')
+            ->join('a.id', 'm')
+            ->where('m.city =' . $request->request->get('city'))
+            ->getQuery()
+            ->getResult();
 
-        $errors = $this->validator->validate($mission);
-
-        if (count($errors) > 0) {
-            $errorsString = (string) $errors;
-            return new JsonResponse($errorsString);
-        }
-
-        $this->em->persist($mission);
-        $this->em->flush();
-
-        return $mission;
+        return $missionsByCity;
     }
 }
